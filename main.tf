7258f5871e90e673652ec495bdaf9874e3ab47d1
Ubuntu <ubuntu@ip-172-31-1-158.ap-southeast-1.compute.internal>
2025-12-02 14:02:56 +0000

    pre-final test

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   }
 
 
-# 0. Memasukan ingatan terraform ke s3 bucket
+# 0. Memasukan ingatan terraform ke s3 bucket via cloud
 backend "s3" {
   bucket = "terraform.state-secops"
   key	 = "jenkins/terraform.tfstate"
