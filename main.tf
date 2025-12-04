e9468b372c3032ab4ce8acc354c1a5306b309a1b
Ubuntu <ubuntu@ip-172-31-1-158.ap-southeast-1.compute.internal>
2025-12-04 01:55:24 +0000

    musti berhasil

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "aws_security_group" "secops_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # ip public gueh sajah atau ubah untuk testing
+    cidr_blocks = ["103.188.169.160/32"] # ip public gueh sajah atau ubah untuk testing, aku nak mbg super
   }
 
   # TRIVY IGNORE: kita masih butuh internet untuk update/install package
